0be841e083064710f2c6257c2c66f3d9db01e0a6
orange <115888595+bitterteasweetorange@users.noreply.github.com>
2023-01-26 05:50:30 +0800

    feat: eslint & prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
-import styles from "./index.module.css";
-import { type NextPage } from "next";
-import Head from "next/head";
-import Link from "next/link";
+import styles from "./index.module.css"
+import { type NextPage } from "next"
+import Head from "next/head"
+import Link from "next/link"
 
-import { api } from "../utils/api";
+import { api } from "../utils/api"
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const hello = api.example.hello.useQuery({ text: "from tRPC" })
 
   return (
     <>
@@ -50,7 +50,7 @@ const Home: NextPage = () => {
         </div>
       </main>
     </>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
