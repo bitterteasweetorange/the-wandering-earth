951ed8e8dda0d6b6f8c102ea1d11c6d7f5c97256
orange <115888595+bitterteasweetorange@users.noreply.github.com>
2023-01-26 22:37:13 +0800

    feat: card

--- src/component/card.tsx
+++ src/component/card.tsx
@@ -1,3 +1,11 @@
+import {
+  alpha,
+  Box,
+  Button,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material"
 import { type Character } from "@prisma/client"
 import { api } from "../utils/api"
 
@@ -13,20 +21,59 @@ export function Card({
       await refetch()
     },
   })
+
+  const theme = useTheme()
+  const match = useMediaQuery("(max-width:900px)")
   return (
-    <div>
+    <Box
+      sx={{
+        display: "grid",
+        alignContent: "start",
+        gridTemplateColumns: match ? "400px" : "400px 400px",
+        justifyContent: "center",
+        gap: 4,
+        height: "calc(100vh - 144px)",
+        overflow: "scroll",
+        padding: 4,
+      }}
+    >
       {data.map((card) => (
-        <div key={card.id}>
-          <h1>{card.name}</h1>
-          <button
+        <Box
+          key={card.id}
+          sx={{
+            height: "200px",
+            padding: 4,
+            position: "relative",
+            background: alpha(theme.palette.background.paper, 0.8),
+            borderRadius: 4,
+            "&:hover": {
+              "& >button": {
+                visibility: "visible",
+              },
+            },
+          }}
+        >
+          <Typography color="error.main" variant="h4">
+            {card.name}
+          </Typography>
+          <Typography color="primary.main" variant="body1">
+            {card.description}
+          </Typography>
+          <Button
             onClick={() => {
               mutate.mutate(card.id)
             }}
+            sx={{
+              position: "absolute",
+              top: 0,
+              right: 0,
+              visibility: "hidden",
+            }}
           >
             delete
-          </button>
-        </div>
+          </Button>
+        </Box>
       ))}
-    </div>
+    </Box>
   )
 }
